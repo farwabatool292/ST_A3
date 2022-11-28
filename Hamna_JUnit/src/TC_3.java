a1d7cdcd2c818e8b535d43edb098f91c05a9a814
HamnaArifeen123 <93051277+HamnaArifeen123@users.noreply.github.com>
2022-11-29 02:20:49 +0500

    Update TC_3.java

--- Hamna_JUnit/src/TC_3.java
+++ Hamna_JUnit/src/TC_3.java
@@ -1,16 +1,17 @@
-package TCPackage_1;
-
+import org.junit.Test;
 import org.openqa.selenium.By;
+
+import static org.junit.Assert.*;
+
 import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
 
 
-public class TC_6 {
+public class TC_3 {
 	  WebDriver driver;
 			@BeforeMethod
 			public void setUp()
